4dbbc3c39927542fa4148e52219dd322e951dbd5
Joan Currie <jecurrie8@gmail.com>
2018-08-25 19:27:25 +0000

    JavaScript RockPaperScissor Game

---
+++ rockPaperScissors.js
@@ -0,0 +1,55 @@
+let userInput = 'Rock';
+userInput = userInput.toLowerCase();
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  
+  switch(randomNumber){
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 02:
+      return 'scissors';
+  }
+}
+  
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === 'bomb'){
+    return 'You won with that BOMB!';  
+  }
+  if (userChoice ===computerChoice){
+    return 'Result is a tie';  
+  }
+  if (userChoice === 'rock'){
+    if (computerChoice === 'paper') {
+      return 'Computer won!';
+    } else {
+      return 'You win!';
+    }; 
+  };  
+  if (userChoice === 'paper'){
+    if (computerChoice === 'scissor') {
+      return 'Computer won!';
+    } else {
+      return 'You win!';
+    };     
+  };  
+  if (userChoice === 'scissors'){
+    if (computerChoice === 'rock') {
+      return 'Computer won!';
+    } else {
+      return 'You win!';
+    };
+  };
+} 
+
+function playGame() {
+  let userChoice = userInput;
+  let computerChoice = getComputerChoice();
+  console.log(userChoice);
+  console.log(computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
